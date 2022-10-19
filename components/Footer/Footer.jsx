7b4adc30cefaed50f196bcf656577838090eb722
hossam255 <hossam255@gmail.com>
2022-10-20 00:52:39 +0200

    Add icons

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -2,19 +2,20 @@ import React from 'react'
 import css from './Footer.module.css'
 import Image from 'next/image'
 import Logo from '../../assets/logos.png'
+import { UilFacebook, UilGithub, UilInstagram } from '@iconscout/react-unicons'
 
 function Footer() {
   return (
     <div className={css.container}>
       <span>ALL RIGHT RESERVED</span>
       <div className={css.social}>
-      <i class ="fa fa-facebook-official fa-3x" aria-hidden="true"/> 
-      <i class="fa fa-github fa-3x" aria-hidden="true"/>
-      <i class="fa fa-instagram fa-3x" aria-hidden="true"/>
+        <UilFacebook/>
+        <UilGithub/>
+        <UilInstagram/>
       </div>
 
       <div className={css.logo}>
-        <Image src={Logo} alt="" width={100} height={50}/>
+        <Image src={Logo} alt="" width={70} height={35}/>
         
       </div>
     </div>
